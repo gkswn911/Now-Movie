aba81532eeea19892d2573835480818e92a2a2a3
gkswn911 <gkswn911@naver.com>
2021-07-06 16:48:00 +0900

    Add : First init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+  
+app.get('/', function (req, res) {
+  // res.send('Hello World!');
+});
+
+app.get('/search', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  
+  console.log('Server is working : PORT - ',port);
+});
+
